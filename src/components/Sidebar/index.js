bda2a383ba53a745477fcf25d2e73cfd64a6eb2a
cw-reece <christopher.w.reece@gmail.com>
2022-06-05 13:40:09 -0500

    style sidebar components to include txt

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,7 +3,7 @@ import './index.scss'
 import LogoS from '../../Assets/images/logo-s.png'
 import LogoSubtitle from '../../Assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => {
   return (
@@ -16,11 +16,21 @@ const Sidebar = () => {
         <NavLink exact="true" activeclassname="active" to="/">
           <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
         </NavLink>
-        <NavLink exact="true" activeclassname="active" to="/">
-          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className={'about-link'}
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
         </NavLink>
-        <NavLink exact="true" activeclassname="active" to="/">
-          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className={'contact-link'}
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
         </NavLink>
       </nav>
     </div>
